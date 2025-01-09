52f7bbbb22b20f05719d05ca1975e38bd4cfbeff
FEDELIS SHARON <139975700+fedelesharon@users.noreply.github.com>
2025-01-09 09:57:45 +0300

    Apis

--- tasks/urls.py
+++ tasks/urls.py
@@ -4,9 +4,11 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from .views import RegisterView
 
 urlpatterns = [
     path('tasks/', TaskListView.as_view(), name='task-list'),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('register/', RegisterView.as_view(), name='register'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -7,6 +7,27 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
 from .models import Task
 from .serializers import TaskSerializer
+from rest_framework import generics
+from django.contrib.auth.models import User
+from rest_framework.permissions import AllowAny
+from rest_framework import serializers
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User(**validated_data)
+        user.set_password(validated_data['password'])  # Hash the password
+        user.save()
+        return user
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]  # Allow any user to register
 
 class TaskListCreateView(APIView):
     permission_classes = [IsAuthenticated]
